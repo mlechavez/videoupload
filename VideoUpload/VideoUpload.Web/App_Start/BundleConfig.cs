a2641df73b313eda6a7e022002efb9d9c78bbdc0
Mark Lester Echavez <lester.echavez@gmail.com>
2017-09-07 15:46:52 +0300

    Remove since. Used the client-side packages

--- VideoUpload/VideoUpload.Web/App_Start/BundleConfig.cs
+++ VideoUpload/VideoUpload.Web/App_Start/BundleConfig.cs
@@ -15,17 +15,7 @@ namespace VideoUpload.Web
             //Layout page
             bundles.Add(
                 new StyleBundle("~/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
-                        .Include("~/Content/bootstrap.css"));
-            //bundles.Add(
-            //    new StyleBundle("~/united", "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css")
-            //            .Include("~/Content/modified/bootstrap.css"));
-
-            //will be removed later
-            //bundles.Add(
-            //    new StyleBundle("~/fontawesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css")
-            //            .Include("~/Content/font-awesome.css"));
-
-            //bundles.Add(new StyleBundle("~/gfont", "https://fonts.googleapis.com/css?family=Raleway:200,300,500"));
+                        .Include("~/Content/bootstrap.css"));            
             
             bundles.Add(
                 new StyleBundle("~/css")
@@ -38,22 +28,7 @@ namespace VideoUpload.Web
                 new StyleBundle("~/cust-layout")
                         .Include(
                             "~/Content/cust-layout.css"));
-
-            bundles.Add(
-                new ScriptBundle("~/jquery", "https://code.jquery.com/jquery-3.2.1.min.js")
-                        .Include("~/scripts/jquery-{version}.js"));
-
-            bundles.Add(
-                new ScriptBundle("~/jbootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")
-                        .Include("~/scripts/bootstrap.js"));
-
-            bundles.Add(
-                new ScriptBundle("~/jcookies", "https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js")
-                        .Include("~/scripts/jquery.cookie.js"));
-
-            bundles.Add(
-                new ScriptBundle("~/tinymce", "https://cloud.tinymce.com/stable/tinymce.js?apiKey=5p70a8d0ew9m48r9xmcngtz3t0itd0czyhvrsiimy476qizj")
-                    .Include("~/scripts/tinymce.js"));
+            
 
             bundles.Add(
                 new ScriptBundle("~/js")
