deef5f1c852e581bc46a79be37f34304e284c185
Mark Lester Echavez <lester.echavez@gmail.com>
2017-09-07 14:26:43 +0300

    Updated the ToClientTime to Give 12-hour time settings

--- VideoUpload/VideoUpload.Web/Common/Extensions.cs
+++ VideoUpload/VideoUpload.Web/Common/Extensions.cs
@@ -21,10 +21,10 @@ namespace VideoUpload.Web.Common
       {
         var offset = int.Parse(timeoffSet.ToString());
         dt = dt.AddMinutes(-1 * offset);
-        return dt.ToString("MMM dd, yyyy hh:mm:ss");
+        return dt.ToString("MMM dd, yyyy hh:mm:ss tt");
       }
       //if there is no offset in session return the datetime in server timezone
-      return dt.ToLocalTime().ToString("MMM dd, yyyy hh:mm:ss");
+      return dt.ToLocalTime().ToString("MMM dd, yyyy hh:mm:ss tt");
     }
 
     public static string HyphenUrl(this HtmlHelper htmlHelper, string actionName)
