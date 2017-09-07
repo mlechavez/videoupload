8e278b4abc5cca16dd0bf36fb9b7cbcdbb425d53
Mark Lester Echavez <lester.echavez@gmail.com>
2017-09-07 14:25:39 +0300

    Added Query For UserId and Type

--- VideoUpload/VideoUpload.Core/Repositories/IHistoryRepository.cs
+++ VideoUpload/VideoUpload.Core/Repositories/IHistoryRepository.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using VideoUpload.Core.Entities;
 
 namespace VideoUpload.Core.Repositories
 {
-    public interface IHistoryRepository : IRepository<History>
-    {
-    }
+  public interface IHistoryRepository : IRepository<History>
+  {
+    List<History> FindHistoriesByUserIDAndType(string userId, string sendingType);
+    Task<List<History>> FindHistoriesByUserIDAndTypeAsync(string userId, string sendingType);
+  }
 }
--- VideoUpload/VideoUpload.EF/Repositories/HistoryRepository.cs
+++ VideoUpload/VideoUpload.EF/Repositories/HistoryRepository.cs
@@ -9,10 +9,20 @@ using VideoUpload.Core.Repositories;
 
 namespace VideoUpload.EF.Repositories
 {
-    internal class HistoryRepository : Repository<History>, IHistoryRepository
+  internal class HistoryRepository : Repository<History>, IHistoryRepository
+  {
+    public HistoryRepository(AppDbContext context) : base(context)
     {
-        public HistoryRepository(AppDbContext context) : base(context)
-        {
-        }
     }
+
+    public List<History> FindHistoriesByUserIDAndType(string userId, string sendingType)
+    {
+      return Set.Where(u => u.UserID == userId && u.Type == sendingType).OrderByDescending(x=>x.DateSent).ToList();
+    }
+
+    public Task<List<History>> FindHistoriesByUserIDAndTypeAsync(string userId, string sendingType)
+    {
+      return Set.Where(u => u.UserID == userId && u.Type == sendingType).OrderByDescending(x => x.DateSent).ToListAsync();
+    }
+  }
 }
