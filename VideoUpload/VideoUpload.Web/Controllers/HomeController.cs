82fe72684e37dba6d45606aca2ffbd7b4cfcc4dd
Mark Lester Echavez <lester.echavez@gmail.com>
2017-10-15 17:30:25 +0300

    Updated showroom contact details

--- VideoUpload/VideoUpload.Web/Controllers/HomeController.cs
+++ VideoUpload/VideoUpload.Web/Controllers/HomeController.cs
@@ -1,53 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using VideoUpload.Core;
 using VideoUpload.Web.Models.Home;
 
 namespace VideoUpload.Web.Controllers
 {
-  [AllowAnonymous]  
-  public class HomeController : Controller
-  {
-    private readonly IUnitOfWork _uow;
-    public HomeController(IUnitOfWork uow)
+    [AllowAnonymous]
+    public class HomeController : Controller
     {
-      _uow = uow;
+        private readonly IUnitOfWork _uow;
+        public HomeController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact-us")]
+        public ActionResult Contact(string branchName)
+        {
+            ViewBag.Header = "Contact us";
+
+            if (!string.IsNullOrWhiteSpace(branchName))
+            {
+                var branch = _uow.Branches.GetByName(branchName);
+                return View(branch);
+            }
+
+            ViewBag.Header = "Contact us";
+
+            return View();
+        }
+
+        public PartialViewResult GetBranches(string videoUrl)
+        {
+            ViewBag.BranchList = ContactWidgetViewModel.Create(_uow, 1, 100);
+            return PartialView("_ContactUsWidget");
+        }
     }
-
-    public ActionResult Index()
-    {
-      return View();
-    }
-
-    [Route("about")]
-    public ActionResult About()
-    {
-      return View();
-    }
-
-    [Route("contact-us")]
-    public ActionResult Contact(string branchName)
-    {
-      ViewBag.Header = "Contact us";
-
-      if (!string.IsNullOrWhiteSpace(branchName))
-      {
-        var branch = _uow.Branches.GetByName(branchName);
-        return View(branch);
-      }
-
-      ViewBag.Header = "Contact us";
-
-      return View();
-    }
-
-    public PartialViewResult GetBranches(string videoUrl)
-    {
-      ViewBag.BranchList = ContactWidgetViewModel.Create(_uow, 1, 100);
-      return PartialView("_ContactUsWidget");
-    }
-  }
 }
