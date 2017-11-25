fc272a95878d8f4f88fb7bc40a902b02152c592b
Mark Lester Echavez <lester.echavez@gmail.com>
2017-11-25 17:32:00 +0300

    updated design ui

--- VideoUpload/VideoUpload.Core/Repositories/IJobcardRepository.cs
+++ VideoUpload/VideoUpload.Core/Repositories/IJobcardRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VideoUpload.Core.Entities;
+﻿using VideoUpload.Core.Entities;
 
 namespace VideoUpload.Core.Repositories
 {
