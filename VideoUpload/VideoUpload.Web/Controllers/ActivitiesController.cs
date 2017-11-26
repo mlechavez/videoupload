14451c0f91d698eab529be6cf0f3783d62b9852a
Mark Lester Echavez <lester@alboraq.local>
2017-11-26 17:51:46 +0300

    updated design UI

--- VideoUpload/VideoUpload.Web/Controllers/ActivitiesController.cs
+++ VideoUpload/VideoUpload.Web/Controllers/ActivitiesController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using VideoUpload.Core;
 using VideoUpload.Core.Entities;
